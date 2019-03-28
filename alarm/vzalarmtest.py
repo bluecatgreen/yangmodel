7108dad9ef7d17506534e9f79634a6e458e6ffed
Raman PoE <bluecatgreen@gmail.com>
2019-03-28 02:08:20 +0000

    Code using pybind generated files

--- alarm/vzalarmtest.py
+++ alarm/vzalarmtest.py
@@ -1,7 +1,21 @@
-from vzalarmrw import alarm_system
+from openconfig_system import openconfig_system
 import pyangbind.lib.pybindJSON as pybindJSON
+import json
 
-alarms  = alarm_system()
-alarm = alarms.alarm_system.alarms.alarm.add("123232") 
-alarm.state.layer="L1";
-print (pybindJSON.dumps(alarms,mode="ietf"))
+oc_system  = openconfig_system()
+oc_system.system.config.hostname = 'CN6508P-7169'
+alarm = oc_system.system.alarms.alarm.add("123232")
+alarm.state.severity  = 'MINOR'
+alarm.state.resource  = 'ETTP-1-3-3'
+alarm.state.text  = 'T-UAS-PCS'
+alarm.state.time_created  = 723423232 
+alarm.state.type_id  = 'EQPT' 
+alarm.state.alarm_ref_id  = '010000000000-0000-0000' 
+alarm.state.type  = 'ALARM' 
+alarm.state.CardType  = 'PTS MRO I/F|| 2xSFP+/14xSFP' 
+alarm.state.AID  = 'ETTP' 
+alarm.state.direction  = 'RCV' 
+alarm.state.location  = 'NEND' 
+alarm.state.ServiceEffect  = 'NON_SERVICE_AFFECTION' 
+print (pybindJSON.dumps(oc_system))
+
